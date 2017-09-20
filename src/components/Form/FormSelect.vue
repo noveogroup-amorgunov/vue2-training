c30ecd2a6c436fadde2e87d3076e65392996f280
Aleksandr Morgunov <aleksandr.morgunov@noveogroup.com>
2017-09-21 01:39:26 +0700

    Add FormSelect component

---
+++ src/components/Form/FormSelect.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="form-item" :class="{ 'form-error': hasError, open: show }">
+    <label v-if="hasError" class="form-label" :for="name">{{ errorText }}</label>
+    <label v-else class="form-label" :for="name">{{ label }}</label>
+
+    <div class="form-select" @click="toggle($event)">
+      {{ selected }}
+      <span class="dropdown-caret"><i class="icon-down"></i></span>
+    </div>
+    <ul class="form-select-dropdown">
+      <li
+        v-for="option in options"
+        v-bind:key="option.value"
+        @click="select(option)"
+        class="dropdown-item">
+        {{ option.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapGetters('app', ['isOpenDropdowns']),
+    },
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      defaultValue: {
+        type: String,
+        default: '',
+      },
+      type: String,
+      label: String,
+      hasError: Boolean,
+      placeholder: String,
+      errorText: String,
+      options: Array,
+      onChange: Function,
+    },
+    data() {
+      return {
+        show: false,
+        selected: 'Default',
+        selectedValue: '',
+      };
+    },
+    watch: {
+      selected() {
+        this.onChange(this.selectedValue);
+      },
+      isOpenDropdowns(value) {
+        if (!value) {
+          this.show = false;
+        }
+      },
+    },
+    methods: {
+      ...mapActions('app', ['closeDropdowns', 'openDropdowns']),
+      toggle(e) {
+        this.show = !this.show;
+        if (this.show) {
+          this.openDropdowns();
+          e.stopPropagation();
+        } else {
+          this.closeDropdowns();
+        }
+      },
+      select(option) {
+        this.show = false;
+        this.selected = option.name;
+        this.selectedValue = option.value;
+      }
+    },
+  };
+</script>
